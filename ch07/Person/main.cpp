1ec0a894852798f862487bd9939c128181d0c03b
wenmingxing1 <1793701192@qq.com>
2017-12-20 13:05:28 +0800

    使用构造函数进行函数构造

--- ch07/Person/main.cpp
+++ ch07/Person/main.cpp
@@ -5,10 +5,22 @@ using namespace std;
 
 int main()
 {
-    Person person;
-    cout << "Please input person's name and address:" << endl;
-    read(cin, person);
-    print(cout, person);
+    Person person1;  //会调用默认构造函数
+    Person person2("ligang", "dongchencun");    //会调用成员初始化列表的构造函数
+
+    cout << "Please input person3's name and address(init the person3):" << endl;
+    Person person3(cin);    //会调用第三个构造函数
+    //Person person2;
+    cout << "Please input person's name and address(init the person1):" << endl;
+    read(cin, person1);
+    //if (read(read(cin, person), person2)); //可以连续读入两个对象的语句
+    print(cout, person1);
+    cout << endl;
+    print(cout, person2);
+    cout << endl;
+    print(cout, person3);
+    cout << endl;
+
 
     return 0;
 }
