5ba26974635fea4b2e859fe129e11c7523510ff1
wenmingxing1 <1793701192@qq.com>
2017-12-25 20:08:44 +0800

    增加文件输出
    输出到文件中

--- ch08/ex8_06/main.cpp
+++ ch08/ex8_06/main.cpp
@@ -7,7 +7,10 @@ using namespace std;
 int main(int argc, char** argv)
 {
     argv[1] = "./data/book.txt";
+    argv[2] = "./data/out.txt";
     ifstream input(argv[1]);    //use "./data/book.txt"
+    ofstream output(argv[2]);   //每次重新写入，不跟随之前，有截断
+    //ofstream output(argv[2], ofstream::app);    //设定模式为追加到文件末尾，这样每次写入不刷新
 
     Sales_data total;
     if (read(input, total)) {
@@ -16,11 +19,11 @@ int main(int argc, char** argv)
             if (total.isbn() == trans.isbn())
                 total.combine(trans);
             else {
-                print(cout, total);  //<< endl;
+                print(output, total) << endl;
                 total = trans;
             }
         }
-        print(cout, total);
+        print(output, total) << endl;
     }
     else {
         cerr << "No data!" << endl;
