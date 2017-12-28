40c1044c171aa5d03bfc3512c3f385508c21b58d
wenmingxing1 <1793701192@qq.com>
2017-12-28 21:24:28 +0800

    算法利用lambda表达式得到可变参数

---
+++ ch10/ex10_16.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+
+using namespace std;
+
+//É¾³ýÖØ¸´ÔªËØ
+void elimdups(vector<string>& vs) {
+    sort(vs.begin(), vs.end());
+    auto new_end = unique(vs.begin(), vs.end());
+    vs.erase(new_end, vs.end());
+}
+
+void biggies(vector<string>& vs, size_t sz) {
+    elimdups(vs);
+
+    stable_sort(vs.begin(), vs.end(),
+                [](const string& str1, const string& str2){return str1.size()<str2.size();});
+
+    auto wc = partition(vs.begin(), vs.end(),
+                        [sz](const string& str) {return str.size() >= sz;});
+
+    int num = 0;
+    cout << "all the letter whose length more than " << sz << " are : " << endl;
+    for (auto iter = vs.begin(); iter != wc; ++iter){
+        ++num;
+        cout << *iter << " ";
+    }
+    cout << endl << "the number of letters is :" << num << endl;
+}
+
+int main()
+{
+    ifstream ifs("./data/input.txt");
+    vector<string> vec;
+    string word;
+    while (ifs >> word)
+        vec.push_back(word);
+
+    biggies(vec, 3);
+
+    return 0;
+
+
+}
+
