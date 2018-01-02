3cd5dd146d3694bdd7aa0bc82e2bf0d75e102b03
wenmingxing1 <1793701192@qq.com>
2018-01-02 20:34:02 +0800

    拷贝构造函数&拷贝赋值运算符&析构函数

--- ch13/ex13_05.cpp
+++ ch13/ex13_05.cpp
@@ -10,6 +10,15 @@ public:
 
     //拷贝构造函数
     HasPtr(const HasPtr& Has) : ps(new string(*Has.ps)), i(0) {}
+
+    //拷贝赋值运算符
+    HasPtr& operator=(const HasPtr& Has) {
+        ps = new string(*Has.ps);
+        i = Has.i
+        return *this;   //记住返回值
+    }
+
+    ~HasPtr() {delete ps;}  //析构函数
 private:
     string *ps;
     int i;
